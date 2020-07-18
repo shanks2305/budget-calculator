b00a68e2f887e30f1e69eb7938ffdca441d52cdf
shanks2305 <contactshishirbahuguna@gmail.com>
2020-07-18 14:49:12 +0530

    Updates

---
+++ precache-manifest.2a84414462b5d67a57712d1084e166ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed6de012781fd2df0a1cf5a632a6beff",
+    "url": "/budget-calculator/index.html"
+  },
+  {
+    "revision": "9f4f529a78bdbf9feae9",
+    "url": "/budget-calculator/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7c33a0431b355dbbe5db",
+    "url": "/budget-calculator/static/css/main.af1147f8.chunk.css"
+  },
+  {
+    "revision": "9f4f529a78bdbf9feae9",
+    "url": "/budget-calculator/static/js/2.3c3e118c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/budget-calculator/static/js/2.3c3e118c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c33a0431b355dbbe5db",
+    "url": "/budget-calculator/static/js/main.01c060db.chunk.js"
+  },
+  {
+    "revision": "9309f97b44e762ed6df3",
+    "url": "/budget-calculator/static/js/runtime-main.f1335b9c.js"
+  }
+]);
